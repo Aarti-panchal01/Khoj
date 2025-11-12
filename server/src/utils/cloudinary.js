b5232fa922cbdde9e5ad813498ab21722fba9887
Aarti Panchal <aartipanchal539@gmail.com>
2025-11-12 12:02:46 +0530

    Update cloudinary.js

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,4 +1,6 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,5 +8,14 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'khoj-items',
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+  },
+});
 
+const upload = multer({ storage });
+
+module.exports = { cloudinary, upload };
